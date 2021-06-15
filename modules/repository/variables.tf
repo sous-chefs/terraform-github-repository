743a107599d3097f61c8dcdf270cb5c3ec9f8342
Jason Field <jason@avon-lea.co.uk>
2021-06-15 13:35:40 +0100

    Repo API logic
    This commit will setup the logic required for repo creation
    it adds in the single API that supports the 4 types of repositories we have

---
+++ modules/repository/variables.tf
@@ -0,0 +1,77 @@
+variable "name" {
+  type = string
+}
+
+variable "supermarket_name_override" {
+  default = ""
+  type    = string
+}
+
+variable "projects_enabled" {
+  type = bool
+  default = false
+}
+variable "repo_type" {
+  type = string
+  validation {
+    condition     = can(regex("^cookbook|terraform|ide|other$", var.repo_type))
+    error_message = "The repo_type must be cookbook, terraform, ide or other. Case sensitive."
+  }
+}
+variable "description_override" {
+  type    = string
+  default = ""
+}
+variable "homepage_url_override" {
+  type    = string
+  default = ""
+}
+variable "additional_topics" {
+  type    = list(string)
+  default = []
+}
+variable "additional_status_checks" {
+  type    = list(string)
+  default = []
+}
+
+
+locals {
+  // supermarket_name
+  supermarket_name = var.supermarket_name_override == null ? var.name : var.supermarket_name_override
+}
+
+locals {
+  // status checks only
+  default_status_checks    = ["mdl", "yamllint"]
+  chef_status_checks       = var.repo_type == "cookbook" ? ["delivery", "integration"] : []
+  terraform_status_checks  = var.repo_type == "terraform" ? ["terraform-lint", "Terraform Cloud/sous-chefs/${var.name}"] : []
+  additional_status_checks = var.additional_status_checks != null ? var.additional_status_checks : []
+  status_checks            = distinct(compact(concat(local.default_status_checks, local.chef_status_checks, local.terraform_status_checks, local.additional_status_checks)))
+
+}
+
+locals {
+  // topics only
+  default_topics = ["terraform-managed"]
+
+  chef_topics       = var.repo_type == "cookbook" ? ["chef", "chef-cookbook", "chef-resource", "${replace(replace(local.supermarket_name, "_", "-"), ".", "")}", "hacktoberfest"] : []
+  ide_topics        = var.repo_type == "ide" ? ["ide", "${replace(replace(var.name, "_", "-"), ".", "")}"] : []
+  terraform_topics  = var.repo_type == "terraform" ? ["terraform", "${replace(replace(var.name, "_", "-"), ".", "")}"] : []
+  additional_topics = var.additional_topics != null ? var.additional_topics : []
+  topics            = distinct(compact(concat(local.default_topics, local.chef_topics, local.ide_topics, local.terraform_topics, local.additional_topics)))
+}
+
+locals {
+  // description only
+  chef_description      = var.repo_type == "cookbook" ? "Development repository for the ${local.supermarket_name} cookbook" : ""
+  ide_description       = var.repo_type == "ide" ? "Development repository for the ${var.name} ide plugin" : ""
+  terraform_description = var.repo_type == "terraform" ? "Configuration repository for the ${var.name} terraform code" : ""
+  description           = var.description_override != null ? var.description_override : join("", [local.chef_description, local.ide_description, local.terraform_description])
+}
+
+locals {
+  // homepage_url
+  chef_homepage_url = var.repo_type == "cookbook" ? "https://supermarket.chef.io/cookbooks/${local.supermarket_name}" : ""
+  homepage_url      = var.homepage_url_override != null ? var.homepage_url_override : local.chef_homepage_url
+}

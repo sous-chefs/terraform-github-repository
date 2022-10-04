cf4cab98438f61f6b0e331329f4a8f20ce52466e
Jason Field <jason@avon-lea.co.uk>
2022-10-04 10:57:26 +0800

    feat(provider): Move to Tf 1.3.1
    Updates the code to support the optional object implementation from
    terraform post 1.3.0 release
    
    Signed-off-by: Jason Field <jason@avon-lea.co.uk>

--- provider.tf
+++ provider.tf
@@ -1,14 +1,13 @@
 terraform {
   // Enabled so we can have optionals in our objects,
   // see: https://github.com/hashicorp/terraform/issues/19898
-  experiments = [module_variable_optional_attrs]
   required_providers {
     github = {
       source  = "integrations/github"
       version = "~> 4.0"
     }
   }
-  required_version = ">= 1.0.0"
+  required_version = "~> 1.3.0"
 
   backend "remote" {
     organization = "sous-chefs"
